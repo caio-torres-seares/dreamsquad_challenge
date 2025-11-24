b29d0d055976061b8b4c663c2a2283e186ee0fba
Caio Torres Seares <caiot.seares@gmail.com>
2025-11-24 01:30:58 -0300

    refactor(core): renomeando variáveis de ambiente para um nome mais intuitivo

--- app/core/config.py
+++ app/core/config.py
@@ -5,9 +5,9 @@ from pydantic_settings import BaseSettings
 load_dotenv()
 
 class Settings(BaseSettings):
-    MODEL_BACKEND: str = os.getenv("MODEL_BACKEND", "ollama")
+    LLM_PROVIDER: str = os.getenv("LLM_PROVIDER", "default")
     OLLAMA_HOST: str = os.getenv("OLLAMA_HOST", "http://localhost:11434")
-    LLM_MODEL: str = os.getenv("LLM_MODEL", "llama3.1:8b")
+    LLM_MODEL_ID: str = os.getenv("LLM_MODEL_ID", "default3.1")
 
 
 settings = Settings()
--- app/core/model_factory.py
+++ app/core/model_factory.py
@@ -3,15 +3,15 @@ from strands.models.ollama import OllamaModel
 
 def build_model():
     
-    if settings.MODEL_BACKEND == "ollama":
+    if settings.LLM_PROVIDER == "ollama":
         return OllamaModel(
             host=settings.OLLAMA_HOST,
-            model_id=settings.LLM_MODEL,
+            model_id=settings.LLM_MODEL_ID,
         )
     
-    if settings.MODEL_BACKEND == "another_backend":
+    if settings.LLM_PROVIDER == "another_model":
         # Caso queira utilizar outro modelo, implemente aqui
         # OBS: Não esqueça de adicionar a importação necessária no topo do arquivo
         pass
 
-    raise ValueError(f"Modelo {settings.MODEL_BACKEND} desconhecido ou não implementado!")
+    raise ValueError(f"Ocorreu um erro ao criar o Modelo '{settings.LLM_PROVIDER}'. Modelo desconhecido ou não implementado!")
